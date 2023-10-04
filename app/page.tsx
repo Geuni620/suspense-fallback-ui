9f4feed1d228844908e668548dd35de15feaeb10
이근휘 <dlrmsgnl620@naver.com>
2023-10-04 17:27:19 +0900

    remove dynamic import

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-// import UsequeryComp from 'app/components/UsequeryComp';
+import UsequeryComp from 'app/components/UsequeryComp';
 import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
 
-const UsequeryComp = dynamic(() => import('./components/UsequeryComp'), {
-  ssr: false,
-  loading: () => <div>useQuery loading...</div>,
-});
+// const UsequeryComp = dynamic(() => import('./components/UsequeryComp'), {
+//   ssr: false,
+//   loading: () => <div>useQuery loading...</div>,
+// });
 
 export default function Home() {
   return (
